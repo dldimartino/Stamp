2ef49f2fb924f97602724aaef5b5ccea13e4f8f8
Dan DiMartino <dandimartino@gmail.com>
2020-09-30 19:23:50 -0400

    fixed hover/focus. working on hamburger

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let hamburgerFunc = () => {
+    let hamburger = document.getElementById("hamburgerItems")
+    if (hamburger.style.display === "off") {
+        hamburger.style.display === "on"
+    } else {
+        hamburger.style.display = "off"
+    }
+}
